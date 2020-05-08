a433f780affcae8c229f3f6bf56e23d859d13b5f
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2020-05-08 11:41:27 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const core = require('@actions/core');
+const fs = require('fs');
+
+try {
+  const htmlPath = core.getInput('html_path');
+  const baseHref = core.getInput('base_href');
+  console.log(`Attempting to rewrite base href in '${htmlPath}' to value '${baseHref}'...`);
+
+  const originalText = fs.readFileSync(htmlPath);
+  const updatedText = originalText
+    .replace(/<base ([^>]*href=["'])([^'"]*)(["'][^>]*)>/, `<base $1${baseHref}$3>`);
+
+  if (originalText !== updatedText) {
+    fs.writeFileSync(htmlPath, updatedText);
+    console.log('Done');  
+  } else {
+    console.warn(`WARNING: no <base> tag with 'href' attribute was found in '${htmlPath}'.`);
+  }
+} catch (error) {
+  core.setFailed(error.message);
+}
