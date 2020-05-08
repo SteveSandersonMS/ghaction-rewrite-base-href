5b54862a8831e012d4f1a8b2660894415fdde8ec
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2020-05-08 11:56:02 +0100

    Fix

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ try {
   const baseHref = core.getInput('base_href');
   console.log(`Attempting to rewrite base href in '${htmlPath}' to value '${baseHref}'...`);
 
-  const originalText = fs.readFileSync(htmlPath);
+  const originalText = fs.readFileSync(htmlPath, 'utf8');
   const updatedText = originalText
     .replace(/<base ([^>]*href=["'])([^'"]*)(["'][^>]*)>/, `<base $1${baseHref}$3>`);
 
